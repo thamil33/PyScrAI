fa91f94e1589922b31663ef16fc3dc764319a594
thamil33 <tyler.hamilton4814@outlook.com>
2025-05-25 03:32:43 -0400

    removed agno

--- agno/ango_my_examples/agents/embed.py
+++
@@ -1,9 +0,0 @@
-from sentence_transformers import SentenceTransformer
-from sentence_transformers import CrossEncoder
-
-
-crossencorder_rerank_model= CrossEncoder("cross-encoder/ms-marco-MiniLM-L6-v2")
-textembed_model = SentenceTransformer("multi-qa-distilbert-cos-v1")
-
-
-
